6d0b9ae61e2760b574dd39113b8c09813d9e638c
Mark Benson <thealternator89@gmail.com>
2021-02-14 10:40:25 +1300

    Change icon for `unescape-bash-newlines` to a '$'
    $ is a common prefix character for (non-elevated) shells

--- Plugins/src/unescape-bash-newlines.ts
+++ Plugins/src/unescape-bash-newlines.ts
@@ -16,7 +16,7 @@ export = {
     id: 'unescape-bash-newlines',
     author: 'thealternator89',
     swishVersion: '1.0.0',
-    icon: 'monitor',
+    icon: 'attach_money',
     process: async (args: ProvidedPluginArgument) => {
         const trimmed = trimLines(args.textContent);
         return trimmed.replace(escapedNewlineRegex, '');
