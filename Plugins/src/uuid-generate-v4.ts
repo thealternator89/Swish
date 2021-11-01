65fd5fda0485e7af683bd3854e0e14f83e5169d1
Mark Benson <thealternator89@gmail.com>
2021-11-01 19:07:09 +1300

    Rename `uuid-generate` to `uuid-generate-v4`

--- Plugins/src/uuid-generate-v4.ts
+++ Plugins/src/uuid-generate-v4.ts
@@ -3,14 +3,13 @@ import { v4 } from 'uuid';
 
 export = {
     name: 'Generate UUID',
-    description:
-        'A base plugin which does nothing (a NO-OP). Copy this to build a plugin.',
-    id: 'uuid-generate',
+    description: 'Generate a V4 UUID',
+    id: 'uuid-generate-v4',
     author: 'thealternator89',
     swishVersion: '1.0.0',
     group: 'UUID',
     usableFrom: ['core', 'clip', 'gui'],
-    process: async (args: ProvidedPluginArgument) => {
+    process: async (_args: ProvidedPluginArgument) => {
         return v4();
     },
 };
