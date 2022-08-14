d383ba1c8febca7a2b675df02466d29362ebad0f
Mark Benson <thealternator89@gmail.com>
2022-08-14 12:20:30 +1200

    Add keyboard navigation to Swish-Gui
    Resolves #1

--- Gui/src/app/palette-dialog/palette-dialog.component.ts
+++ Gui/src/app/palette-dialog/palette-dialog.component.ts
@@ -1,18 +1,27 @@
-import { Component, NgZone, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, NgZone, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { ActiveDescendantKeyManager } from '@angular/cdk/a11y';
+
 import { MatDialogRef } from '@angular/material/dialog';
 import { PluginDefinition } from 'swish-base';
 import { IpcService } from '../ipc.service';
+import { PalettePluginItemComponent } from '../palette-plugin-item/palette-plugin-item.component';
 
 @Component({
   selector: 'app-palette-dialog',
   templateUrl: './palette-dialog.component.html',
   styleUrls: ['./palette-dialog.component.scss'],
 })
-export class PaletteDialogComponent implements OnInit {
+export class PaletteDialogComponent {
   plugins: PluginDefinition[];
+
+  @ViewChildren(PalettePluginItemComponent)
+  pluginComponents!: QueryList<PalettePluginItemComponent>;
+
+  keyManager?: ActiveDescendantKeyManager<PalettePluginItemComponent>;
 
   constructor(
     public dialogRef: MatDialogRef<PaletteDialogComponent>,
+    private changeDetector: ChangeDetectorRef,
     private ngZone: NgZone,
     private ipc: IpcService
   ) {
@@ -21,8 +30,11 @@ export class PaletteDialogComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
     this.onSearchChange('');
+    this.keyManager = new ActiveDescendantKeyManager(
+      this.pluginComponents
+    ).withWrap();
   }
 
   close() {
@@ -34,10 +46,25 @@ export class PaletteDialogComponent implements OnInit {
   onSearchChange(query) {
     this.ipc.searchPlugins(query).then((results: PluginDefinition[]) => {
       this.plugins = results;
+
+      // Ensure we update the UI before we try to set the first item as active
+      this.changeDetector.detectChanges();
+      this.keyManager!.setFirstItemActive();
     });
   }
 
-  selectPlugin(plugin) {
-    this.dialogRef.close(plugin);
+  selectPlugin(item?: PalettePluginItemComponent) {
+    this.dialogRef.close(item?.plugin);
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.selectPlugin(this.keyManager?.activeItem!);
+    }
+    this.keyManager?.onKeydown(event);
+  }
+
+  focusPlugin(item: PalettePluginItemComponent) {
+    this.keyManager?.setActiveItem(item);
   }
 }
