31060f8537301319d2cad81d216adee74e79d71d
Mark Benson <41453195+thealternator89@users.noreply.github.com>
2023-02-05 15:50:19 +1300

    Add dev mode so hot reloading works for Swish-Gui
    Run npm run dev to launch in dev mode

--- Gui/electron/main.ts
+++ Gui/electron/main.ts
@@ -5,8 +5,13 @@ import { buildMenus } from './menu-manager';
 
 import * as path from 'path';
 import * as url from 'url';
+import { env } from 'process';
 
 const APP_NAME = 'Swish';
+
+const DEV_MODE = !!env["SWISH_DEVELOPMENT"];
+
+const sleep = (duration) => new Promise((resolve) => setTimeout(resolve, duration));
 
 const platformWindowOptions = () => {
   // Hide the titlebar and
@@ -58,14 +63,36 @@ function createWindow() {
 
   ipcHandler.setWindow(win);
 
-  win.loadURL(
-    url
-      .pathToFileURL(path.join(__dirname, '../../../dist/swish-gui/index.html'))
-      .toString()
-  );
+  // If in dev mode, attempt to load the app from the standard angular location
+  // Otherwise load the compiled angular app
+  if(DEV_MODE) {
+    console.log('Running in Dev Mode - will attempt to use ng for live development');
+    safeLoadUrl("http://localhost:4200");
+  }
+  else {
+    win.loadURL(
+      url
+        .pathToFileURL(path.join(__dirname, '../../../dist/swish-gui/index.html'))
+        .toString()
+      );
+  }
 
   win.on('closed', () => {
     win = null;
     ipcHandler.setWindow(null);
   });
+}
+
+// Attempt to load a URL in the window
+// If it fails, wait a second and try again.
+async function safeLoadUrl(url:string, logOnError:boolean = true) {
+  try {
+    await win.loadURL(url);
+  } catch (error) {
+    if(logOnError) {
+      console.error(`Failed to load URL: ${url}. Ng may still be building. Will retry every second.`);
+    }
+    await sleep(1000);
+    await safeLoadUrl(url, false);
+  }
 }
