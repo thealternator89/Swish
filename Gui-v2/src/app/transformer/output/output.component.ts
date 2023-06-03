9ad981656ca1024f3970e3c54548743806d5f92b
Mark Benson <41453195+thealternator89@users.noreply.github.com>
2023-06-03 18:41:45 +1200

    Dynamic tab ordering

--- Gui-v2/src/app/transformer/output/output.component.ts
+++ Gui-v2/src/app/transformer/output/output.component.ts
@@ -13,20 +13,79 @@ export class OutputComponent implements OnInit {
   @ViewChild('outputMessage')
   outputMessage: OutputMessageComponent;
 
-  outputType: 'none' | 'message' | 'text' | 'markdown' | 'html' = 'none';
+  tabs: string[] = [];
+
+  outputType: 'none' | 'message' | 'tabs' = 'none';
   output: PluginResult = null;
 
   ngOnInit(): void {}
 
   handlePluginResult(result: PluginResult) {
-    if (result.render) {
-      this.outputType = result.render;
-    } else if (result.text) {
-      this.outputType = 'text';
+    if (result.render || result.text) {
+      this.outputType = 'tabs';
+      this.tabs = this.getTabs(result);
     } else {
       this.outputType = 'none';
     }
 
     this.output = result;
+  }
+
+  tabName(key: string) {
+    switch (key) {
+      case 'text': {
+        const language = this.syntaxName(this.output.syntax);
+        if (language) {
+          return `Text (${language})`;
+        }
+        return 'Text';
+      }
+      case 'markdown':
+        return 'Markdown';
+      case 'html':
+        return 'HTML';
+      default:
+        return key;
+    }
+  }
+
+  private syntaxName(language: string) {
+    switch (language) {
+      case 'text':
+      case 'plaintext':
+      case undefined:
+        return '';
+      case 'json':
+      case 'xml':
+      case 'yaml':
+        return language.toLocaleUpperCase();
+      default:
+        return this.capitalize(language);
+    }
+  }
+
+  private capitalize(value: string) {
+    return value ? value.charAt(0).toUpperCase() + value.slice(1) : '';
+  }
+
+  private getTabs(result: PluginResult) {
+    const tabs = [];
+
+    // If the plugin specifies a type to render, use that as the first tab
+    if (result.render) {
+      tabs.push(result.render);
+    }
+
+    // Look for other types of output and add them as tabs if they aren't already there
+    if (result.text) {
+      tabs.includes('text') || tabs.push('text');
+    }
+    if (result.markdown) {
+      tabs.includes('markdown') || tabs.push('markdown');
+    }
+    if (result.html) {
+      tabs.includes('html') || tabs.push('html');
+    }
+    return tabs;
   }
 }
