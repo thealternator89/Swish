08a50fdfeaa763a96c312c58b557303c160ecc7c
Mark Benson <41453195+thealternator89@users.noreply.github.com>
2023-06-08 17:34:35 +1200

    Fix path detection when in production mode (For showing/hiding buttons on titlebar)

--- Gui-v2/src/app/titlebar/titlebar.component.ts
+++ Gui-v2/src/app/titlebar/titlebar.component.ts
@@ -6,22 +6,25 @@ import { IpcService } from '../ipc.service';
 @Component({
   selector: 'app-titlebar',
   templateUrl: './titlebar.component.html',
-  styleUrls: ['./titlebar.component.scss']
+  styleUrls: ['./titlebar.component.scss'],
 })
 export class TitlebarComponent {
-
   title = 'Swish';
   platform: string;
 
-  constructor(private _router: Router, private _notifier: NotifierService, ipc: IpcService) {
+  constructor(
+    private _router: Router,
+    private _notifier: NotifierService,
+    ipc: IpcService
+  ) {
     this.platform = ipc.platform;
   }
 
   // Show the back button if the current URL is not the same as the referrer
-  showHomeButton = () => new URL(document.URL).pathname !== '/';
+  showHomeButton = () => this._router.url !== '/';
 
   // Only show the settings button if the current URL is the home page
-  showSettingsButton = () => new URL(document.URL).pathname === '/';
+  showSettingsButton = () => this._router.url === '/';
 
   goHome(): void {
     this._router.navigateByUrl('/');
@@ -42,10 +45,12 @@ export class TitlebarComponent {
 
   getPlatformClass(): string {
     switch (this.platform) {
-      case 'win32': return 'win';
-      case 'darwin': return 'mac';
-      default: return 'linux';
+      case 'win32':
+        return 'win';
+      case 'darwin':
+        return 'mac';
+      default:
+        return 'linux';
     }
   }
-
 }
